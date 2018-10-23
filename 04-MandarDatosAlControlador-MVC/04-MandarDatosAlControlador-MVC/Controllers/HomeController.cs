a1fcdc121c049e793cb022af397263a341d3a347
adeborja <adeborja@users.noreply.github.com>
2018-10-23 14:43:48 +0200

    23/10/2018

---
+++ 04-MandarDatosAlControlador-MVC/04-MandarDatosAlControlador-MVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using _04_MandarDatosAlControlador_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04_MandarDatosAlControlador_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        /// <summary>
+        /// Primera peticion de la vista Editar
+        /// Creamos un objeto de clase clsPersona y lo enviamos
+        /// </summary>
+        /// <returns>un objeto tipo clsPersona</returns>
+        public ActionResult Editar()
+        {
+            clsPersona p1 = new clsPersona(1, "Angel", "deBo", new DateTime(11/8/1987), "Calle Lejos", "123456789");
+
+            return View(p1);
+        }
+
+        /// <summary>
+        /// Envio del usuario submit/post con los datos modificados de la persona
+        /// </summary>
+        /// <param name="p"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public ActionResult Editar(clsPersona p)
+        {
+            //Investigar como llamar a otra vista y pasarle el objeto
+
+            return View();
+        }
+    }
+}
---
+++ 04-MandarDatosAlControlador-MVC/04-MandarDatosAlControlador-MVC/Models/clsPersona.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _04_MandarDatosAlControlador_MVC.Models
+{
+    public class clsPersona
+    {
+        #region constructor por defecto
+
+        public clsPersona()
+        {
+            
+        }
+
+        #endregion
+
+        #region constructor por parametros
+
+        public clsPersona(int nIdPersona, String nNombre, String nApellido, DateTime nFechaNacimiento, String nDireccion, String nTelefono)
+        {
+            this.idPersona = nIdPersona;
+            this.nombre = nNombre;
+            this.apellidos = nApellido;
+            this.fechaNacimiento = nFechaNacimiento;
+            this.direccion = nDireccion;
+            this.telefono = nTelefono;
+        }
+
+        #endregion
+
+
+        #region atributos y propiedades
+
+        public int idPersona { get; set; }
+        public String nombre { get; set; }
+        public String apellidos { get; set; }
+        public DateTime fechaNacimiento { get; set; }
+        public String direccion { get; set; }
+        public String telefono { get; set; }
+
+        #endregion
+    }
+}
