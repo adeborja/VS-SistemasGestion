941d56e61d173a87a87ab095b43d030138c7cd27
adeborja <adeborja@users.noreply.github.com>
2018-10-30 22:14:39 +0100

    gv hbj

---
+++ 05-QueryEnMethodGet/05-QueryEnMethodGet/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _05_QueryEnMethodGet.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        //public ActionResult Index()
+        //{
+        //    bool esMiPrimeraVez = true;
+        //    ViewData["esMiPrimeraVez"] = true;
+
+        //    return View(esMiPrimeraVez);
+        //}
+
+        public ActionResult Index(bool? esMiPrimeraVez)
+        {
+            if(esMiPrimeraVez == null)
+                ViewData["esMiPrimeraVez"] = true;
+            else
+                ViewData["esMiPrimeraVez"] = false;
+
+
+            return View();
+        }
+    }
+}
