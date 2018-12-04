cdb1bd4420d379396a1406a85dbda6849d934a9c
adeborja <adeborja@users.noreply.github.com>
2018-12-04 14:39:04 +0100

    4/12/2018
    Hacer interfaz para hacerlo todo en una sola pagina

---
+++ 10-HelloWorld-AJAX/10-HelloWorld-AJAX/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_10_HelloWorld_AJAX.MvcApplication" Language="C#" %>
---
+++ 10-HelloWorld-AJAX/10-HelloWorld-AJAX/Scripts/metodosAJAX.js
@@ -0,0 +1,34 @@
+﻿//manuales>javascript>ultimo
+
+window.onload = inicializaEventos;
+
+function inicializaEventos() {
+
+    document.getElementById("btnLlamada").addEventListener("click", llamada, false);
+}
+
+function llamada()
+{
+    //alert('Wola Mundo');
+    var miLlamada = new XMLHttpRequest();
+
+    //miLlamada.open("GET", "/Home/Index");
+    miLlamada.open("GET", "https://angelapirestpersonas.azurewebsites.net/api/personas/1");
+
+    //Mientras viene
+    miLlamada.onreadystatechange = function () {
+
+        alert(miLlamada.readyState);
+
+        //mientras se espera a que se terminen los pasos anteriores
+        if (miLlamada.readyState < 4) {
+            document.getElementById("textoMostrar").innerHTML = "Cargando...";
+        }
+        //cuando ya han terminado
+        else if (miLlamada.readyState == 4 && miLlamada.status == 200) {
+            document.getElementById("textoMostrar").innerHTML = miLlamada.responseText;
+        }
+    };
+
+    miLlamada.send();
+}
